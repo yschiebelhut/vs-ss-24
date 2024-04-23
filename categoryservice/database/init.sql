0a99a3532f0d934c510e6751121b19d5a3c64c23
hotu1011 <h_tuandat@yahoo.de>
2024-04-23 11:43:35 +0200

    add tables for category

--- categoryservice/database/init.sql
+++ categoryservice/database/init.sql
@@ -1,1 +1,11 @@
-# Initializes the Database with all necessary tables
+#
+Initializes the Database with all necessary tables
+
+CREATE TABLE category
+(
+    id   INT          NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE INDEX FK1mtsbur82frn64de7balymq9s ON product (category_id ASC);
